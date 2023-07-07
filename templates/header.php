607db7d93b6570045a997485ac946398481e2cb8
lqasemzadeh <lqasemzadeh@gmail.com>
2023-07-07 19:09:01 +0330

    add a pizza

--- templates/header.php
+++ templates/header.php
@@ -10,13 +10,19 @@
         .brand-text{
             color: #cbb09c !important;
         }
+        form{
+            max-width: 460px;
+            margin: 20px auto;
+            padding: 20px;
+        }
     </style>
 </head>
   <body class="grey lighten-4">
-    <nav class="white z-depth-0">
+  <nav class="white z-depth-0">
         <div class="container">
             <a href="#" class="brand-logo brand-text">HotPizza</a>
             <ul id="nav-mobile" class="right hide-on-small-and-down">
                 <li><a href="#" class="btn brand z-depth-0">Add a Pizza</a></li>
             </ul>
         </div>
+  </nav>
