783363c5fad43e346f5bbcfa725b07f1afbf488d
lqasemzadeh <lqasemzadeh@gmail.com>
2023-07-18 19:45:43 +0330

    connect to database

--- index.php
+++ index.php
@@ -2,6 +2,22 @@
 
 // connect to database
 $conn = mysqli_connect('localhost', 'shaun', 'test123', 'ninja_pizza');
+
+// check connection
+if(!$conn){
+    echo 'Connection erro: ' . mysqli_connect_error();
+}
+
+// write query for all pizzas
+$sql = 'SELECT title, ingredients, id FROM pizzas';
+
+// make query & get result
+$result = mysqli_query($conn, $sql);
+
+//fetch the resulting rows as an array
+$pizzas = mysqli_fetch_all($result, MYAQLI_ASSOC);
+
+print_r($pizzas);
 ?>
 
 <!DOCTYPE html>
